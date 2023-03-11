fb9821e13fc175f5eeefbcb8786a542c7cb54db1
liuyuancheng <tenstone@foxmail.com>
2023-03-11 21:02:51 +0800

    fix

--- cluster/examples/client.rs
+++ cluster/examples/client.rs
@@ -1,4 +1,5 @@
 use std::marker::PhantomData;
+use std::task::{Context, Poll};
 use std::time::Duration;
 
 use futures::pin_mut;
@@ -6,15 +7,26 @@ use rand::rngs::mock;
 use tokio::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use net::Address;
+use net::dial::DefaultMakeTransport;
 
 use tower::{BoxError};
+use tower::make::MakeConnection;
 
 struct Transport{
-    _marker: PhantomData<>
+    socket: TcpStream,
+    _marker: PhantomData<TcpStream>
 }
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), BoxError> {
+    let transport = DefaultMakeTransport::new();
+    let mut conn = transport
+        .make_connection(Address::Ip("127.0.0.1:8858".parse().unwrap()))
+        .await
+        .unwrap();
     let socket = TcpStream::connect("127.0.0.1:1111").await?;
     let (mut rd, mut wr) = io::split(socket);
 
